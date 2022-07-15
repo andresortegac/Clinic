e720c6e6139db712733c1824f0a35680db5f8ac6
Andres Ortega <andres.ortegac97@gmail.com>
2022-07-15 08:54:40 -0500

    creación del componente user

---
+++ src/Components/Users.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Users = () => {
+    return(
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Id_user</th>
+                    <th>Name_user</th>
+                    <th>Password_user</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Id_user</th>
+                    <th>Name_user</th>
+                    <th>Password_user</th>
+                </tr>
+            </tbody>
+
+        </table>
+    );
+}
+
+export default Users;
